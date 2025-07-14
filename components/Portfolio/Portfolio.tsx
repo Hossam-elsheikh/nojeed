501299bc87f08511ac5c35640a420891df85444b
mustafa <mustafamhmod95@gmail.com>
2025-07-15 02:28:11 +0300

    add carousels

--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -1,31 +1,74 @@
+'use client'
+
 import React from 'react'
 import SectionHeader from '../ui/SectionHeader'
 import PortfolioCard from './PortfolioCard'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+import 'swiper/css/pagination'
+
+import { Pagination } from 'swiper/modules'
+const projects = [
+    {
+        title: 'Reservation Hotel System',
+        description: 'A user-friendly hotel booking system...',
+        imgSrc: 'portfolio-image.png',
+    },
+    {
+        title: 'Reservation Hotel System',
+        description: 'A user-friendly hotel booking system...',
+        imgSrc: 'portfolio-image.png',
+    },
+    {
+        title: 'Estudee Dashboard',
+        description: 'An interactive dashboard for tracking tasks...',
+        imgSrc: 'portfolio-image.png',
+    },
+    {
+        title: 'Invoice Manager',
+        description: 'A simple invoice management tool...',
+        imgSrc: 'portfolio-image.png',
+    },
+    {
+        title: 'Estudee Dashboard',
+        description: 'An interactive dashboard for tracking tasks...',
+        imgSrc: 'portfolio-image.png',
+    },
+
+    // Add more projects here
+]
 
 const Portfolio = () => {
     return (
-        <section className='py-20 mb-20 tracking-wide'>
+        <section className="pt-20 pb-[6rem] tracking-wide">
             <SectionHeader
                 title="Portfolio"
                 subTitle="From SaaS platforms to business automation tools"
                 additionalClassesSubTitle=""
             />
             <div className="flex justify-between mt-[5.25rem] !gap-7">
-                <PortfolioCard
-                    title="Reservation Hotel System"
-                    description="A user-friendly hotel booking system that enables seamless reservation management and a smooth customer experience."
-                    imgSrc="portfolio-image.png"
-                />
-                <PortfolioCard
-                    title="Estudee Dashboard"
-                    description="An interactive dashboard for tracking tasks and projects with a modern and comfortable interface."
-                    imgSrc="portfolio-image.png"
-                />
-                <PortfolioCard
-                    title="Invoice Manager"
-                    description="A simple invoice management tool that allows efficient creation and tracking of invoices."
-                    imgSrc="portfolio-image.png"
-                />
+                <Swiper
+                    slidesPerView={3}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    initialSlide={1}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    {projects.map((project, index) => (
+                        <SwiperSlide key={index}>
+                            <PortfolioCard
+                                title={project.title}
+                                description={project.description}
+                                imgSrc={project.imgSrc}
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>{' '}
             </div>
         </section>
     )
--- components/Testimonials/Testimonials.tsx
+++ components/Testimonials/Testimonials.tsx
@@ -1,7 +1,30 @@
+'use client'
+
 import React from 'react'
 import SectionHeader from '../ui/SectionHeader'
 import TestimonialCard from './TestimonialCard'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+import 'swiper/css/pagination'
+
+import { Pagination } from 'swiper/modules'
+
 const testimonials = [
+    {
+        text: `Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.`,
+        name: 'ROBERT DOE',
+        image: '/images/testimonials/person.png',
+        rating: 4.5,
+    },
+    {
+        text: `Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.`,
+        name: 'ROBERT DOE',
+        image: '/images/testimonials/person.png',
+        rating: 4.5,
+    },
     {
         text: `Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.`,
         name: 'ROBERT DOE',
@@ -24,16 +47,30 @@ const testimonials = [
 
 const Testimonials = () => {
     return (
-        <div className="py-20 mb-20">
+        <div className="py-28">
             <SectionHeader
                 title="Testimonials"
                 subTitle="What Our Clients Say about working with us"
                 additionalClassesSubTitle="leading-[1.35]"
             />
-            <div className="flex gap-6 mt-[3.25rem]">
-                {testimonials.map((item, index) => (
-                    <TestimonialCard key={index} {...item} />
-                ))}
+            <div className="mt-[3.25rem]">
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    initialSlide={1}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper shrink-0"
+                >
+                    {testimonials.map((item, index) => (
+                        <SwiperSlide key={index}>
+                            {' '}
+                            <TestimonialCard key={index} {...item} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
         </div>
     )
